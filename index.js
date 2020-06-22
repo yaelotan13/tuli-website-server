381df08a0ddc2084c4fa21365c3f961a86f50e2a
yael.lotan <yaelotan19@gmail.com>
2020-06-22 20:51:38 +0300

    fix(index.js): change port to process.env.port

--- index.js
+++ index.js
@@ -31,6 +31,6 @@ app.post('/send', (req, res) => {
   };
 });
 
-app.listen(3000, () => {
-  console.log('server start on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`server start on port ${process.env.PORT || 3000}`);
 });
