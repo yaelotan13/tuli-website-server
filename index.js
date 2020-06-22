e3206c4d531aba86e81e5eb75968ae5f999f63e2
yael.lotan <yaelotan19@gmail.com>
2020-06-22 20:54:55 +0300

    feat(index.js): add log to error

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ app.post('/send', (req, res) => {
     console.log('sending email');
     transporter.sendMail(mailOptions, function(error, info) {
         if (error) {
+            console.log(error);
             res.status(500).send('has error');
         } else {
             res.status(200).send(`<div><h4>Sender Information:</h4><p><b>Name:</b> ${req.body.name}</p><p><b>Email:</b> ${req.body.email}</p><p><b>Phone Number:</b> ${req.body.tel}</p><hr></hr><br></br><p><b>Message:</b><br></br><br></br>${req.body.content}</p></div>`);
